7114f23ddc1d700b0c492d529d003e2718bb0afe
David Montaño <dalx2549@gmail.com>
2016-11-15 20:51:01 -0500

    Add custom validation messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -11,6 +11,6 @@ class Message
   validates :email, presence: true
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: "El email no es válido"
 
-  validates :content, presence => { :message => "Campo requerido" }
+  validates :content, :presence => { :message => "Campo requerido" }
 
 end
